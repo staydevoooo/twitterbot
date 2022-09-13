3b6c162b2436cea6d8231c648954e9412ebbce17
staydevoooo <72087646+staydevoooo@users.noreply.github.com>
2022-09-13 08:26:47 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,65 @@
+const fetch = require("node-fetch");
+var XMLHttpRequest = require('xhr2');
+var xhr = new XMLHttpRequest();
+
+const url = "https://opensheet.elk.sh/1HRo8seXlonLZgRBWDOvAIjhEijzC-hnJJfvl3IRO4tQ/1";
+
+function fetchConfession() {
+  /// call your function here
+  let jsondata;
+  fetch(url).then(
+    function(u) { return u.json(); }
+  ).then(
+    function(json) {
+      jsondata = json;
+      let lastElement = jsondata[jsondata.length - 1];
+      let name = "Name: " + lastElement.Name;
+      let confession = "Confession: " + lastElement.Confession;
+      document.getElementById('name').innerHTML = name;
+      document.getElementById('confession').innerHTML = confession;
+      console.log(name);
+      console.log(confession);
+    }
+  );
+
+};
+fetchConfession();
+
+
+let previous = null;
+let current = null;
+setInterval(function() {
+  let request = new XMLHttpRequest();
+  request.open('GET', url, true);
+
+  request.onload = function() {
+    if (this.status >= 200 && this.status < 400) {
+      let json = JSON.parse(this.response);
+      current = JSON.stringify(json);
+      if (previous && current && previous !== current) {
+        let jsondata;
+        fetch(url).then(
+          function(u) { return u.json(); }
+        ).then(
+          function(json) {
+            jsondata = json;
+            let lastElement = jsondata[jsondata.length - 1];
+            let name = "Name: " + lastElement.Name;
+            let confession = "Confession: " + lastElement.Confession;
+            document.getElementById('name').innerHTML = name;
+            document.getElementById('confession').innerHTML = confession;
+            console.log(name);
+            console.log(confession);
+          }
+        );
+      }
+      previous = current;
+    }
+  };
+
+  request.send();
+}, 10000);
+
+
+/* fetchConfession();
+var fetchInterval = setInterval(fetchConfession, 5000); */
