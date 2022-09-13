fade9d9c0589775629a11bd6a61a4fa51e208c5a
staydevoooo <72087646+staydevoooo@users.noreply.github.com>
2022-09-13 17:54:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import fetch from "node-fetch";
-
+import XMLHttpRequest from "xhr2";
 const url = "https://opensheet.elk.sh/1HRo8seXlonLZgRBWDOvAIjhEijzC-hnJJfvl3IRO4tQ/1";
 
 function fetchConfession() {
