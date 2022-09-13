531ad57c8fd81917451ade352bda0e52a810525a
staydevoooo <72087646+staydevoooo@users.noreply.github.com>
2022-09-13 19:29:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import fetch from "node-fetch";
 import XMLHttpRequest from "xhr2";
 const url = "https://opensheet.elk.sh/1HRo8seXlonLZgRBWDOvAIjhEijzC-hnJJfvl3IRO4tQ/1";
+var port = process.env.PORT || 8080;
 
 function fetchConfession() {
   /// call your function here
@@ -52,7 +53,8 @@ setInterval(function() {
 
   request.send();
 }, 10000);
-
+var server=app.listen(port,function() {
+console.log("app running on port 8080"); });
 
 
 /* fetchConfession();
