e0b2f368d4733cffcb21b0432f2c4b85293bb070
staydevoooo <72087646+staydevoooo@users.noreply.github.com>
2022-09-13 18:56:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ function fetchConfession() {
 };
 fetchConfession();
 
-
 let previous = null;
 let current = null;
 setInterval(function() {
@@ -53,6 +52,15 @@ setInterval(function() {
 
   request.send();
 }, 10000);
+
+app.set('port', (process.env.PORT || 5000));
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 /* fetchConfession();
 var fetchInterval = setInterval(fetchConfession, 5000); */
