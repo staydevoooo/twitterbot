14391f62deae8888e1fd8023d320534045d0d3e4
staydevoooo <72087646+staydevoooo@users.noreply.github.com>
2022-09-13 19:32:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import fetch from "node-fetch";
 import XMLHttpRequest from "xhr2";
 const url = "https://opensheet.elk.sh/1HRo8seXlonLZgRBWDOvAIjhEijzC-hnJJfvl3IRO4tQ/1";
-var port = process.env.PORT || 8080;
 
 function fetchConfession() {
   /// call your function here
@@ -22,39 +21,10 @@ function fetchConfession() {
 };
 fetchConfession();
 
-let previous = null;
-let current = null;
-setInterval(function() {
-  let request = new XMLHttpRequest();
-  request.open('GET', url, true);
 
-  request.onload = function() {
-    if (this.status >= 200 && this.status < 400) {
-      let json = JSON.parse(this.response);
-      current = JSON.stringify(json);
-      if (previous && current && previous !== current) {
-        let jsondata;
-        fetch(url).then(
-          function(u) { return u.json(); }
-        ).then(
-          function(json) {
-            jsondata = json;
-            let lastElement = jsondata[jsondata.length - 1];
-            let name = "Name: " + lastElement.Name;
-            let confession = "Confession: " + lastElement.Confession;
-            console.log(name);
-            console.log(confession);
-          }
-        );
-      }
-      previous = current;
-    }
-  };
 
   request.send();
 }, 10000);
-var server=app.listen(port,function() {
-console.log("app running on port 8080"); });
 
 
 /* fetchConfession();
